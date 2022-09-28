ba2a42fd95ce489f72ceacad607168182b59dd34
rohini kushwaha <rohiniee0028@gmail.com>
2022-09-29 01:26:00 +0530

    pushed day-2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,42 @@
 import { Box, Button, Container, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { BsList, BsSearch } from "react-icons/bs";
 import { FaEnvelopeOpenText, FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import SimpleImageSlider from "react-simple-image-slider";
 import styles from "./Navbar.module.css";
+import Sections from "./Sections";
 
 export const Navbar = () => {
 
-const timeElapsed = Date.now();
-const today = new Date(timeElapsed);
-const x = today.toUTCString()
-console.log(x)
+    const images = [
+        { url: "https://tpc.googlesyndication.com/simgad/12692791964622314734" },
+        { url: "https://tpc.googlesyndication.com/simgad/12692791964622314734" },
+        { url: "images/3.jpg" },
+    ];
+    // const ImageSlider = () => {
+    //     return (
+    //         <div>
+    //             <SimpleImageSlider
+    //                 width={896}
+    //                 height={504}
+    //                 images={images}
+    //                 showBullets={true}
+    //                 showNavs={true}
+    //             />
+    //         </div>
+    //     );
+    // }
+
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    const x = today.toUTCString()
+    // console.log(x)
 
     return (
-        <Container maxW='90%' mt="10px" style={{ display:"flex",flexDirection:"column",gap:"20px"}}>
+        <Container maxW='90%' mt="10px" style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
             <Box style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <Flex display="flex" gap="15px" alignItems="center">
-                    <Box bg="#EFEFEF" px="0.75rem" py=".5rem" borderRadius="5px" className={styles.sections}>
-                        <Flex alignItems="center" gap="5px"><BsList /> Sections</Flex>
+                    <Box >
+                       <Sections />
                     </Box>
                     <Box bg="#EFEFEF" px=".75rem" py=".7rem" borderRadius="5px">
                         <BsSearch />
@@ -38,7 +59,7 @@ console.log(x)
             </Box>
             <Box style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
                 <Box className={styles.date}>
-                    <Text fontSize="12px">date</Text>
+                    <Text fontSize="12px">{x}</Text>
                     <Text fontSize="12px">EPAPER | TODAY’S PAPER</Text>
                 </Box>
                 <Box className={styles.heading}>
@@ -46,31 +67,37 @@ console.log(x)
                     <Text fontSize="13px" fontWeight="bold">JOURNALISM OF COURAGE</Text>
                 </Box>
                 <Box>
-                    slider
+                    <SimpleImageSlider
+                        width={221}
+                        height={71}
+                        images={images}
+                    // showBullets={true}
+                    // showNavs={true}
+                    />
                 </Box>
             </Box>
             <Box borderTop="1px solid lightgray" borderBottom="1px solid lightgray" p="2px 9px" className={styles.refrence}>
                 <Flex justifyContent="space-between" alignItems="center">
                     <Box fontSize="13px" color="black">
                         <HStack gap="10px" className={styles.row3}>
-                            <Text _hover={{color:"#D70000"}}>Home</Text>
-                            <Text _hover={{color:"#D70000"}}>Explained</Text>
-                            <Text _hover={{color:"#D70000"}}>Political Pulse</Text>
-                            <Text _hover={{color:"#D70000"}}>India</Text>
-                            <Text _hover={{color:"#D70000"}}>Cities</Text>
-                            <Text _hover={{color:"#D70000"}}>Opinion</Text>
-                            <Text _hover={{color:"#D70000"}}>Entertainment</Text>
-                            <Text _hover={{color:"#D70000"}}>Lifestyle</Text>
-                            <Text _hover={{color:"#D70000"}}>Technology</Text>
-                            <Text _hover={{color:"#D70000"}}>Videos</Text>
-                            <Text _hover={{color:"#D70000"}}>Sports</Text>
-                            <Text _hover={{color:"#D70000"}}>Audio</Text>
-                            <Text _hover={{color:"#D70000"}}>Education</Text>
-                            <Text _hover={{color:"#D70000"}}>Premium</Text>
-                            <Text _hover={{color:"#D70000"}}>Investigations</Text>
+                            <Text _hover={{ color: "#D70000" }}>Home</Text>
+                            <Text _hover={{ color: "#D70000" }}>Explained</Text>
+                            <Text _hover={{ color: "#D70000" }}>Political Pulse</Text>
+                            <Text _hover={{ color: "#D70000" }}>India</Text>
+                            <Text _hover={{ color: "#D70000" }}>Cities</Text>
+                            <Text _hover={{ color: "#D70000" }}>Opinion</Text>
+                            <Text _hover={{ color: "#D70000" }}>Entertainment</Text>
+                            <Text _hover={{ color: "#D70000" }}>Lifestyle</Text>
+                            <Text _hover={{ color: "#D70000" }}>Technology</Text>
+                            <Text _hover={{ color: "#D70000" }}>Videos</Text>
+                            <Text _hover={{ color: "#D70000" }}>Sports</Text>
+                            <Text _hover={{ color: "#D70000" }}>Audio</Text>
+                            <Text _hover={{ color: "#D70000" }}>Education</Text>
+                            <Text _hover={{ color: "#D70000" }}>Premium</Text>
+                            <Text _hover={{ color: "#D70000" }}>Investigations</Text>
                         </HStack>
                     </Box>
-                    <Box style={{ display: "flex",justifyContent: "center",alignItems: "flex-end",gap: "10px"}}>
+                    <Box style={{ display: "flex", justifyContent: "center", alignItems: "flex-end", gap: "10px" }}>
                         <Button bg="#FFB900" fontSize="14px" colorScheme="#FFB900" color="black">SUBSCRIBE</Button>
                         <Button bg="#3B4057" color="white" fontSize="14px" colorScheme="#3B4057">Sign in</Button>
                     </Box>
@@ -78,14 +105,14 @@ console.log(x)
             </Box>
             <Box borderTop="1px solid lightgray" borderBottom="1px solid lightgray" p="10px 0px " mt="-4" >
                 <Flex gap="20px" justifyContent="center" alignItems="center" className={styles.trending}>
-                  <Text color="#D70000" fontSize="12px" fontWeight="500">TRENDING</Text>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Crossword $ Sudoku</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">UPSC special</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Everyday Explainers</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Health Specials</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Academic Counselling</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Follow Authors</Box>
-                  <Box _hover={{color:"#D70000"}} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Cricket</Box>
+                    <Text color="#D70000" fontSize="12px" fontWeight="500">TRENDING</Text>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Crossword $ Sudoku</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">UPSC special</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Everyday Explainers</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Health Specials</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Academic Counselling</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Follow Authors</Box>
+                    <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Cricket</Box>
                 </Flex>
             </Box>
         </Container>
---
+++ src/Components/Sections.jsx
@@ -0,0 +1,115 @@
+import { Box, Button, Container, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, ListItem, Radio, RadioGroup, Stack, Text, UnorderedList, useDisclosure, VStack } from "@chakra-ui/react"
+import React from "react";
+import { BsList } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
+import styles from "./Navbar.module.css";
+
+function Sections() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [placement, setPlacement] = React.useState('top')
+
+  return (
+    <>
+      {/* <RadioGroup defaultValue={placement} onChange={setPlacement}>
+          <Stack direction='row' mb='4'>
+            <Radio value='top'>Top</Radio>
+            <Radio value='right'>Right</Radio>
+            <Radio value='bottom'>Bottom</Radio>
+            <Radio value='left'>Left</Radio>
+          </Stack>
+        </RadioGroup> */}
+      <Button _hover={{ bg: "#EFEFEF" }} bg="#EFEFEF" onClick={onOpen} gap="10px">
+        <BsList />
+        Section
+      </Button>
+      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='2px' mb="5">
+            <Box w="90%" margin="auto">
+              <Flex justify="space-between" alignItems="center" >
+                <Box>
+                  <ImCross onClick={onClose} style={{ cursor: "pointer" }} />
+                </Box>
+                <Box style={{ display: "flex", gap: "10px" }}>
+                  <Button bg="#FFB900" fontSize="14px" colorScheme="#FFB900" color="black">SUBSCRIBE</Button>
+                  <Button bg="#3B4057" color="white" fontSize="14px" colorScheme="#3B4057">Sign In</Button>
+                </Box>
+              </Flex>
+            </Box>
+          </DrawerHeader>
+          <DrawerBody>
+            <Box w="80%" margin="auto">
+              <HStack justify="space-between" lineHeight="3rem">
+                <VStack>
+                  <UnorderedList style={{listStyle:"none",cursor:"pointer"}}>
+                    <ListItem>e-paper</ListItem>
+                    <ListItem>Premium</ListItem>
+                    <ListItem>India</ListItem>
+                    <ListItem>Cities</ListItem>
+                  </UnorderedList>
+                </VStack>
+                <VStack>
+                  <UnorderedList style={{listStyle:"none" ,cursor:"pointer"}}>
+                    <ListItem>Opinion</ListItem>
+                    <ListItem>Political Pulse</ListItem>
+                    <ListItem>Explained</ListItem>
+                    <ListItem>Science</ListItem>
+                  </UnorderedList>
+                </VStack>
+                <VStack>
+                  <UnorderedList style={{listStyle:"none",cursor:"pointer"}}>
+                    <ListItem>Sports</ListItem>
+                    <ListItem>World</ListItem>
+                    <ListItem>Business</ListItem>
+                    <ListItem>Entertainment</ListItem>
+                  </UnorderedList>
+                </VStack>
+                <VStack>
+                  <UnorderedList style={{listStyle:"none",cursor:"pointer"}}>
+                    <ListItem>Health</ListItem>
+                    <ListItem>Lifestyle</ListItem>
+                    <ListItem>Technology</ListItem>
+                    <ListItem>Education</ListItem>
+                  </UnorderedList>
+                </VStack>
+                <VStack>
+                  <UnorderedList style={{listStyle:"none",cursor:"pointer"}}>
+                    <ListItem>Eye</ListItem>
+                    <ListItem>Trending</ListItem>
+                    <ListItem>Investigations</ListItem>
+                    <ListItem>Newsletters</ListItem>
+                  </UnorderedList>
+                </VStack>
+                <VStack>
+                  <UnorderedList style={{listStyle:"none",cursor:"pointer"}}>
+                    <ListItem>Photos</ListItem>
+                    <ListItem>Videos</ListItem>
+                    <ListItem>Audio</ListItem>
+                    <ListItem>Web Stories</ListItem>
+                  </UnorderedList>
+                </VStack>
+              </HStack>
+            </Box>
+          </DrawerBody>
+          <DrawerHeader borderTopWidth='2px' mt="5">
+            <Box  p="10px 0px " mt="-3" >
+              <Flex gap="20px" justifyContent="center" alignItems="center" className={styles.trending}>
+                <Text color="#D70000" fontSize="14px" fontWeight="500">TRENDING</Text>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Crossword $ Sudoku</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">UPSC special</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Everyday Explainers</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Health Specials</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Academic Counselling</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Follow Authors</Box>
+                <Box _hover={{ color: "#D70000" }} bg="#EFEFEF" p="2px 12px" borderRadius="5px" fontSize="13px">Cricket</Box>
+              </Flex>
+            </Box>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default Sections;
